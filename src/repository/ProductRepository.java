0600f5403794607244c717cc463cbce2e7b596fb
AmirsinaRashedi <amirsina.rashedi@gmail.com>
2022-05-16 17:54:37 +0430

    updated ProductRepository

--- src/repository/ProductRepository.java
+++ src/repository/ProductRepository.java
@@ -9,7 +9,7 @@ public class ProductRepository {
         this.connection = connection;
     }
 
-    public void printAllProducts() throws SQLException {
+    public int printAllProducts() throws SQLException {
         String selectStarFromProductsQuery = "select * from product as p " +
                 "left join electronics as e on p.product_id = e.product_id " +
                 "left join shoes as s on s.product_id = p.product_id " +
@@ -19,9 +19,12 @@ public class ProductRepository {
 
         ResultSet resultSet = statement.executeQuery(selectStarFromProductsQuery);
 
-        System.out.println("query done!");
+        int maxId = 0;
+
         while (resultSet.next()) {
             System.out.print(resultSet.getInt(1) + "- ");
+
+            maxId = resultSet.getInt(1);
 
             if (resultSet.getString("model") != null)
                 System.out.print(resultSet.getString("model")
@@ -37,6 +40,8 @@ public class ProductRepository {
             System.out.println("price: " + resultSet.getInt(3) + "\t"
                     + "in stock: " + resultSet.getInt(2));
         }
+
+        return maxId;
     }
 
     public void changeStock(int[] idAndQuantity) throws SQLException {
@@ -61,4 +66,51 @@ public class ProductRepository {
 
         preparedStatement.executeUpdate();
     }
+
+    public int checkAvailableAmount(int id) throws SQLException {
+        String selectAvailableUnitsQuarry = "select available_units from product where product_id = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(selectAvailableUnitsQuarry);
+
+        preparedStatement.setInt(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        return resultSet.getInt(1);
+    }
+
+    public int printProductFromCart(int item[]) throws SQLException {
+        String selectStarFromProductQuarry = "select * from product where product_id = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(selectStarFromProductQuarry);
+
+        preparedStatement.setInt(1, item[0]);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        int price = 0;
+
+        while (resultSet.next()) {
+            if (resultSet.getString("model") != null)
+                System.out.print(resultSet.getString("model")
+                        + "    " + resultSet.getString("manufacturer") + "    ");
+            else if (resultSet.getString("shoe_brand") != null)
+                System.out.print(resultSet.getString("shoe_brand")
+                        + "    " + resultSet.getString("shoe_type") + "    ");
+            else if (resultSet.getString("litterateur_title") != null)
+                System.out.print(resultSet.getString("litterateur_title")
+                        + "    " + resultSet.getString("litterateur_type") + "    "
+                        + resultSet.getString("litterateur_author") + "    ");
+
+            price += (resultSet.getInt("price_per_unit") * item[1]);
+        }
+
+        System.out.println(" : " + item[1]);
+
+        return price;
+
+    }
+
 }
